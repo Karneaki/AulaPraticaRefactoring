50a27b32c5d275a70adb5c201a73c7b522cfb603
crysthianSF <crysthiansf@gmail.com>
2024-11-29 22:43:42 -0300

    Commit 12

--- Price.java
+++ Price.java
@@ -1,3 +1,8 @@
 public abstract class Price {
+
     public abstract int getPriceCode();
- }
+
+    public abstract double getCharge(int daysRented);
+
+    public abstract int getFrequentRenterPoints(int daysRented);
+}
